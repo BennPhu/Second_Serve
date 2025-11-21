7dcc3fda7189da8c86d5bf3fc72cb227f8244658
mrodenas <marodenas@asbarcelona.com>
2025-11-21 12:40:14 -0800

    Initial Commit

---
+++ SecondServe/SecondServeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondServeApp.swift
+//  SecondServe
+//
+//  Created by Marc Rodenas Guasch on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SecondServeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SecondServeTests/SecondServeTests.swift
@@ -0,0 +1,16 @@
+//
+//  SecondServeTests.swift
+//  SecondServeTests
+//
+//  Created by Marc Rodenas Guasch on 21/11/25.
+//
+
+import Testing
+
+struct SecondServeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
