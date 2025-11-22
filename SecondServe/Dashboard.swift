9897353ac48cbb6aebc70e6ffeb6d31fa33506d3
mrodenas <marodenas@asbarcelona.com>
2025-11-21 19:32:14 -0800

    Added Icons switching betweeen Dashboard UI and others

--- SecondServe/Dashboard.swift
+++ SecondServe/Dashboard.swift
@@ -1,34 +1,64 @@
-//
-//  ContentView.swift
-//  SecondServe
-//
-//  Created by Marc Rodenas Guasch on 21/11/25.
-//
-
 import SwiftUI
 
 struct Dashboard: View {
     @State private var searchFood = ""
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 20) {
+        TabView {
 
-                // Search Bar
-                TextField("Search", text: $searchFood)
-                    .textFieldStyle(.roundedBorder)
-                    .padding(.horizontal)
-                    .frame(width: 360, height: 47)
+            // HOME TAB — Your Dashboard content
+            ScrollView {
+                VStack(spacing: 20) {
 
-                // Banner
-                Image("PearBanner")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 330, height: 100)
-                    .clipped()
-                    .cornerRadius(12)
+                    // Search Bar
+                    TextField("Search", text: $searchFood)
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.horizontal)
+                        .frame(width: 360, height: 47)
+
+                    // Banner
+                    Image("PearBanner")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 330, height: 100)
+                        .clipped()
+                        .cornerRadius(12)
+                }
+                .padding(.top, 24)
             }
-            .padding(.top, 24)
+            .tabItem {
+                Image(systemName: "house.fill")
+                Text("Home")
+            }
+
+
+            // SELL TAB
+            Text("Sell")
+                .tabItem {
+                    Image(systemName: "dollarsign")
+                    Text("Sell")
+                }
+
+            // DONATION TAB
+            Text("Donation")
+                .tabItem {
+                    Image(systemName: "gift")
+                    Text("Donation")
+                }
+
+            // CART TAB
+            Text("Cart")
+                .tabItem {
+                    Image(systemName: "cart.fill")
+                    Text("Cart")
+                }
+
+            // PROFILE TAB
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
         }
     }
 }
